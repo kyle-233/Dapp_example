f1a862b2dc52fa57a9ee3685e791a267f4642fec
kyle-233 <kyle07142113@163.com>
2023-08-29 08:41:04 +0800

    chore:构建项目

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+    <h1
+      className="text-3xl font-bold underline"
+      onClick={() => { }}
+    >
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
